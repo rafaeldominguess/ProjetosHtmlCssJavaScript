73bc53de7a9abeb82af29515373ac72ce0d2e6dd
RAFAEL DOMINGUES <rafael9997016@gmail.com>
2025-12-25 02:21:00 -0300

    Própria class pokemon
    Criando nossos próprios parametros para atualizar a lista de pokemons

--- Projeto_Pokedex/js/main.js
+++ Projeto_Pokedex/js/main.js
@@ -1,21 +1,14 @@
 
-
 function convertPokemonToLi(pokemon) {
     return `
-    <li class="pokemon">
-                <span class="number">#001</span>
+    <li class="pokemon ${pokemon.type}">
+                <span class="number">#${pokemon.number}</span>
                 <span class="name">${pokemon.name}</span>
                 <div class="detail">
                     <ol class="types">
-                        <li class="type">
-                            grass
-                        </li>
-
-                        <li class="type">
-                            poison
-                        </li>
+                     ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
                     </ol>
-                    <img class="img" src="https://media.tenor.com/XtXuPwRrjoIAAAAi/joia.gif" alt="${pokemon.name}">
+                    <img class="img" src="${pokemon.photo}" alt="${pokemon.name}">
                 </div>
             </li>
             `
@@ -26,5 +19,7 @@ const pokemonList = document.getElementById('pokemonList')
 
 //Trasnforma um nome pokemon em uma string
 pokeApi.getPokemons().then((pokemons = []) => {
-    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
+    const newHtml = pokemons.map(convertPokemonToLi).join('')
+    pokemonList.innerHTML = newHtml
 })
+
